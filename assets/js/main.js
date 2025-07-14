d2170b87f2d657735b30c529c996b8d6ae42b0a0
zhanggy <zhanggy@geoscene.cn>
2025-07-14 10:57:44 +0800

    refactor: jquery rewire

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,28 @@ async function getLayerName(url) {
   const layer = await Layer.fromArcGISServerUrl({ url });
   const { declaredClass } = layer;
   const layerName = declaredClass.split(".").at(-1);
-
   return layerName;
 }
 
 function setResult(layerName) {
-  const resultDiv = document.getElementById("result");
-  const content = `<p>You want the <a href="https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-${layerName}.html" class="link-primary" target="_blank">${layerName}</a>.</p>`;
-  resultDiv.innerHTML = content;
+  const $link = $("<a>", {
+    href: `https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-${layerName}.html`,
+    class: "link-primary",
+    target: "_blank",
+    text: layerName,
+  });
+
+  $("#result")
+    .empty()
+    .append($("<p>").html("You want the ").append($link).append("."));
 }
 
-const btn = document.getElementById("submit");
-btn.addEventListener("click", async () => {
-  const url = document.getElementById("url").value;
+$("#result").hide();
+$("#form").on("submit", async function (e) {
+  e.preventDefault();
+  $("#result").fadeOut("slow");
+  const url = $("#url").val();
   const layerName = await getLayerName(url);
   setResult(layerName);
+  $("#result").fadeIn("slow");
 });
